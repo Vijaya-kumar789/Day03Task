a66fba9861cb8399b1c08243a6702f15c34417a0
vijaya-kumar789 <vijaykumaryoyo8@gmail.com>
2024-01-31 17:10:00 +0530

    This is Day03Task

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//1.Question 
 var obj1 = {name:"person 1",age:5};
 var obj2={age:5,name:"person 1"};
  console.log(JSON.stringify(obj1)===JSON.stringify(obj2) );
--- script1.js
+++ script1.js
@@ -4,10 +4,11 @@ request.send();
 request.onload=function(){
     var res=JSON.parse(request.response);
     console.log(res);
-
+//2.Question
     for (var i=0;i<res.length;i++){
         console.log(res[i].flags.png)
         }
+ //3.Question
          for (var i=0;i<res.length;i++){
         console.log("name:"+res[i].name.common+","+"region:"+res[i].region+","+"subregion:"+res[i].subregion+","+"population:"+res[i].population)
          }
