624a3d249222533ffae174223fac83ce7114ea9c
vijaya-kumar789 <vijaykumaryoyo8@gmail.com>
2024-01-31 16:57:20 +0530

    This is Day03Task

---
+++ script1.js
@@ -0,0 +1,16 @@
+var request = new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v3.1/all");
+request.send();
+request.onload=function(){
+    var res=JSON.parse(request.response);
+    console.log(res);
+
+    for (var i=0;i<res.length;i++){
+        console.log(res[i].flags.png)
+        }
+        
+
+    for (var i=0;i<res.length;i++){
+        console.log("name:"+res[i].name.common+","+"region:"+res[i].region+","+"subregion:"+res[i].subregion+","+"population:"+res[i].population)
+         }
+ }
